d2d0218fa3ac17619f303f968af8c3ab5c14564c
Agus Cahyono <agus.cahyono@hiapp.id>
2019-07-11 16:29:52 +0700

    init files

---
+++ HiCall/HiCall.h
@@ -0,0 +1,19 @@
+//
+//  HiCall.h
+//  HiCall
+//
+//  Created by Agus Cahyono on 11/07/19.
+//  Copyright © 2019 HiApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HiCall.
+FOUNDATION_EXPORT double HiCallVersionNumber;
+
+//! Project version string for HiCall.
+FOUNDATION_EXPORT const unsigned char HiCallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HiCall/PublicHeader.h>
+
+
---
+++ HiCall/HiCall.swift
@@ -0,0 +1,13 @@
+//
+//  HiCall.swift
+//  HiCall
+//
+//  Created by Agus Cahyono on 11/07/19.
+//  Copyright © 2019 HiApp. All rights reserved.
+//
+
+import Foundation
+
+public class HiCall: NSObject {
+    
+}
